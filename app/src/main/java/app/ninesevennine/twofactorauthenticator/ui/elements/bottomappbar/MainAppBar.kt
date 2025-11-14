7de034357504826dee4bdc5092547d1b21d9d14a
Babkov <babkov@979.st>
2025-11-14 14:40:07 +0100

    Fix search bar focus when switching screens

--- app/src/main/java/app/ninesevennine/twofactorauthenticator/ui/elements/bottomappbar/MainAppBar.kt
+++ app/src/main/java/app/ninesevennine/twofactorauthenticator/ui/elements/bottomappbar/MainAppBar.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -101,6 +102,13 @@ fun MainAppBar(
         LaunchedEffect(Unit) {
             focusRequester.requestFocus()
             keyboardController?.show()
+        }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            keyboardController?.hide()
+            focusManager.clearFocus()
         }
     }
 
